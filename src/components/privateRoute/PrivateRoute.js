60e2a75344678f8d27a1c68b99c76f455fd1048a
pariwesh <tamrpariwesh@gmail.com>
2023-03-20 22:53:54 +1100

    restarted project

--- src/components/privateRoute/PrivateRoute.js
+++
@@ -1,5 +0,0 @@
-import Router from "next/router"
-const PrivateRoute = () => {
-  return <div>PrivateRoute</div>
-}
-export default PrivateRoute
--- src/components/smile/Smile.js
+++
@@ -1,6 +0,0 @@
-import { SmileStyles } from "./smileStyles"
-
-const Smile = () => {
-  return <SmileStyles>Smile</SmileStyles>
-}
-export default Smile
